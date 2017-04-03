607b1806c484b6d09cfd821efe050b6245becef9
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2017-04-03 15:37:30 -0400

    add inital commit

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('planning-poker', ['ngRoute','ngResource']);
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/users',{
+            templateUrl: '/views/users.html',
+            controller: 'usersController'
+        })
+        .when('/roles',{
+            templateUrl: '/views/roles.html',
+            controller: 'rolesController'
+        })
+        .otherwise(
+            { redirectTo: '/'}
+        );
+});
+
